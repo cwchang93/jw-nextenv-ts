8ce5e7b505038fc0b2cd3c7b5299bd0977279a17
Jinwei <cwchang93@gmail.com>
2020-02-23 21:36:02 +0800

    Add special Keyboard regex request

--- source/emoji-regex/newTest.js
+++ source/emoji-regex/newTest.js
@@ -15,4 +15,43 @@ const excludeSpecial = (input) => {
     // if (specialCharacter.length < 1) return false
 }
 
-excludeSpecial("今天fdd】s")
+// excludeSpecial("今天fdd】s")
+
+const specialReg = /[\u3002][\uff1f][\uff01][\uff0c][\u3001][\uff1b][\uff1a][\u201c][\u201d][\u2018][\u2019][\uff08][\uff09][\u300a][\u300b][\u3008][\u3009][\u3010][\u3011][\u300e][\u300f][\u300c][\u300d][\u3014][\u3015][\u2026][\u2014][\uff5e]/
+
+
+const specialCharacter = () => {
+
+}
+
+/**
+ * 。	\u3002
+？	\uff1f
+！	\uff01
+，	\uff0c
+、	\u3001
+；	\uff1b
+：	\uff1a
+“	\u201c
+”	\u201d
+‘	\u2018
+’	\u2019
+（	\uff08
+）	\uff09
+《	\u300a
+》	\u300b
+〈	\u3008
+〉	\u3009
+【	\u3010
+】	\u3011
+『	\u300e
+』	\u300f
+「	\u300c
+」	\u300d
+〔	\u3014
+〕	\u3015
+…	\u2026
+—	\u2014
+～	\uff5e
+ * 
+ */
